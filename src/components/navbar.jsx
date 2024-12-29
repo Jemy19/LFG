03cce084087a1c0eb8f2aaf6b516afbef2222f1b
Jemy19 <gellidojeremy@gmail.com>
2024-12-29 19:35:02 +0800

    Add: clerk middlware and nav signout

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,28 +1,37 @@
 import Link from "next/link"
 import { MaxWidthWrapper} from "./max-width-wrapper"
 import { SignOutButton } from "@clerk/nextjs"
-import { Button } from "./ui/button"
+import { Button, buttonVariants } from "./ui/button"
+import { ArrowRight } from "lucide-react"
 
 export const Navbar = () => {
     const user = true
 
     return (
-        <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bh-white/80 backdrop-blur-lg transition-all">
+        <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
             <MaxWidthWrapper>
                 <div className="flex h-16 items-center justify-between">
                     <Link  href="/" className="flex z-40 font-semibold">
                         Ping<span className="text-brand-700">Panda</span>
                     </Link>
-                </div>
+                
 
-                <div className="h-full flex items-center space-x-4">
-                    {user ?
-                    <>
-                        <SignOutButton>
-                            <Button className=""></Button>
-                        </SignOutButton>
-                    </> 
-                    : null}
+                    <div className="h-full flex items-center space-x-4">
+                        {user ?
+                        <>
+                            <SignOutButton>
+                                <Button size="sm" variant="ghost">Sign Out</Button>
+                            </SignOutButton>
+
+                            <Link href="/dashboard" className={buttonVariants({
+                                size: "sm",
+                                className: "flex items-center gap-1"
+                            })}>
+                            Dashboard <ArrowRight className="ml-1.5 size-4"/>   
+                            </Link>
+                        </> 
+                        : null}
+                    </div>
                 </div>
             </MaxWidthWrapper>
         </nav>
