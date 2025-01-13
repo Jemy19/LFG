1ca0dd17a817499ef81865df98ec3fcb405bfc19
Jemy19 <gellidojeremy@gmail.com>
2025-01-13 16:40:43 +0800

    feat: added signIn and signOut

---
+++ src/app/(auth)/sign-in/[[...rest]]/page.tsx
@@ -0,0 +1,13 @@
+"user client"
+
+import { SignIn } from "@clerk/nextjs"
+
+const Page = () => {
+    return (
+    <div className="w-full flex-1 flex items-center justify-center">
+        <SignIn/>
+    </div>
+    )
+}
+
+export default Page
