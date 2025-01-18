e492615abfd9c474b64c9ea1d6c7d95d4703d0a6
Jemy19 <gellidojeremy@gmail.com>
2025-01-18 21:48:50 +0800

    feat: auth backend router connected

--- src/app/(auth)/welcome/page.tsx
+++ src/app/(auth)/welcome/page.tsx
@@ -2,9 +2,18 @@
 
 import { Heading } from "@/components/heading";
 import { LoadingSpinner } from "@/components/loading-spinner";
+import { usePrefetchQuery } from "@tanstack/react-query";
 import { LucideProps } from "lucide-react";
 
 const Page = () => {
+
+    const {} = usePrefetchQuery({
+        queryFn: () => {
+            client
+        }
+
+    })
+
     return (
     <div className="flex w-full flex-1 items-center justify-center px-4">
         <BackgroundPattern className="absolute inset-0 left-1/2 z-0 -translate-x-1/2 opacity-75"/>
