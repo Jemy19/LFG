4b9869f51771fad0264d61776b314569326b075f
Jemy19 <gellidojeremy@gmail.com>
2025-01-22 19:13:26 +0800

    feat: auth middleware | dashboard category

--- src/server/procedures.ts
+++ src/server/procedures.ts
@@ -1,7 +1,38 @@
+import { db } from "@/db"
 import { j } from "./__internals/j"
+import { currentUser } from "@clerk/nextjs/server"
+import { HTTPException } from "hono/http-exception"
 
-const authmiddleware = j.middleware(({next}) => {
-    return next({})
+const authMiddleware = j.middleware(async ({ c, next }) => {
+    const authHeader = c.req.header("Authorization")
+
+    if (authHeader) {
+        const apiKey = authHeader.split(" ")[1] 
+
+        const user = await db.user.findUnique({
+            where: { apiKey },
+        })
+
+        if (user) return next({ user })
+    }
+
+    const auth = await currentUser()
+
+    if (!auth) {
+        throw new HTTPException(401, {message: "Unauthorized"})
+    }
+
+    const user = await db.user.findUnique({
+        where: {
+            externalId: auth.id
+        },
+    })
+
+    if (!user) {
+        throw new HTTPException(401, {message: "Unauthorized"})
+    }
+
+    return next({ user })
 })
 /**
  * Public (unauthenticated) procedures
@@ -10,4 +41,4 @@ const authmiddleware = j.middleware(({next}) => {
  */
 export const baseProcedure = j.procedure
 export const publicProcedure = baseProcedure
-export const privateProcedure = publicProcedure.use(authmiddleware)
+export const privateProcedure = publicProcedure.use(authMiddleware)
