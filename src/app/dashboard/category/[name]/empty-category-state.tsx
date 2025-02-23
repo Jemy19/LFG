38b1e841e4b5fba97ae7438c225bf0acb53cab7d
Jemy19 <gellidojeremy@gmail.com>
2025-02-23 18:39:08 +0800

    feat: ongoing discord bot

--- src/app/dashboard/category/[name]/empty-category-state.tsx
+++ src/app/dashboard/category/[name]/empty-category-state.tsx
@@ -80,6 +80,27 @@ export const EmptyCategoryState = ({ categoryName, } : { categoryName: string}) 
                         {codeSnippet}
                     </SyntaxHighlighter>
             </div>
+
+
+            <div className="mt-8 flex flex-col items-center space-x-2">
+                <div className="flex gap-2 items-center">
+                    <div className="size-2 bg-green-500 rounded-full animate-pulse" />
+                    <span className="text-sm text-gray-600">Listing to incoming events...</span>
+                </div>
+                <p className="text-sm/6 text-gray-600 mt-2">
+                    Need help? Check out our {" "} 
+                    <a href="#" className="text-blue-600 hover:underline">
+                        documentation
+                    </a>
+                    {" "} 
+                        or 
+                    {" "}
+                    <a href="#" className="text-blue-600 hover:underline">
+                        contact support
+                    </a>
+                    .
+                </p>
+            </div>
         </Card>
     )
 }
