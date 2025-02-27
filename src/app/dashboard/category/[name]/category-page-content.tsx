927fdec038a4dd0afac3e56d4109d6f01c9740fd
jemy <gellidojeremy@gmail.com>
2025-02-28 06:07:56 +0800

    feat: ongoing events detail page

--- src/app/dashboard/category/[name]/category-page-content.tsx
+++ src/app/dashboard/category/[name]/category-page-content.tsx
@@ -3,9 +3,13 @@
 import { EventCategory } from "@prisma/client"
 import { useQuery } from "@tanstack/react-query"
 import { EmptyCategoryState } from "./empty-category-state"
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { useSearchParams } from "next/navigation"
 import { client } from "@/lib/client"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Card } from "@/components/ui/card"
+import { BarChart } from "lucide-react"
+import { isAfter, isToday, startOfMonth, startOfWeek } from "date-fns"
 
 interface CategoryPageContentProps {
     hasEvents: boolean
@@ -51,15 +55,131 @@ export const CategoryPageContent = ({hasEvents: initialHasEvents, category }: Ca
                 timeRange: activeTab,
             })
 
-            return await res.json
+            return await res.json()
         },
         refetchOnWindowFocus: false,
         enabled: pollingData.hasEvents,
     })
+    // useMemo used so that it wont recalculate unless page refreshed
+    const numericFieldSums = useMemo(() => {
+        if (!data?.events || data.events.length === 0) return {}
+
+        const sums: Record<
+            string,
+            {
+                total: number
+                thisWeek: number
+                thisMonth: number
+                today: number
+            }> = {}
+
+        const now = new Date()
+        const weekStart = startOfWeek(now, {weekStartsOn: 0})
+        const monthStart = startOfMonth(now)
+
+        data.events.forEach((event) => {
+            const eventDate = event.createdAt
+
+            Object.entries(event.fields as object)
+            .forEach(([field, value]) =>{
+                if(typeof value === "number") {
+                    if(!sums[field]){
+                        sums[field] = {total: 0, thisWeek: 0, thisMonth: 0, today: 0 }
+                    }
+
+                    sums[field].total += value
+
+                    if(isAfter(eventDate, weekStart) || eventDate.getTime() === weekStart.getTime()){
+                        sums[field].thisWeek += value
+                    }
+                    if(isAfter(eventDate, monthStart) || eventDate.getTime() === monthStart.getTime()){
+                        sums[field].thisMonth += value
+                    }
+                    if(isToday(eventDate)){
+                        sums[field].today += value
+                    }
+                }
+            })
+        })
+        return sums
+    }, [data?.events])
+
+    const NumericFieldSumCards = () => {
+        if (Object.keys(numericFieldSums).length === 0) return null
+
+        return Object.entries(numericFieldSums).map(([field, sums]) => {
+            const relevantSum = 
+                activeTab === "today" 
+                ? sums.today 
+                : activeTab === "week"
+                ? sums.thisWeek
+                : sums.thisMonth
+
+                return (
+                    <Card key={field}>
+                        <div className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <p className="text-sm/6 font-medium">
+                                {field.charAt(0).toUpperCase() + field.slice(1)}
+                            </p>
+                            <BarChart className="size-4 text-muted-foreground"/>
+                        </div>
+
+                        <div>
+                            <p className="text-2xl font-bold">
+                                {relevantSum.toFixed(2)}
+                            </p>
+                            <p className="text-xs/5 text-muted-foreground">
+                                {activeTab === "today" 
+                                ? "today" 
+                                : activeTab === "week"
+                                ? "this week"
+                                : "this month"}
+                            </p>
+                        </div>
+                    </Card>
+                )
+        })
+    }
 
     return (
         <div className="space-y-6">
+            <Tabs value={activeTab} onValueChange={(value) => {
+                setActiveTab(value as "today" | "week" | "month")
+            }}>
+                <TabsList>
+                    <TabsTrigger value="today">Today</TabsTrigger>
+                    <TabsTrigger value="week">This Week</TabsTrigger>
+                    <TabsTrigger value="month">This Month</TabsTrigger>
+                </TabsList>
+                <TabsContent value={activeTab}>
+                        <div className="grid grid-cols-1 md: grid-cols-2 lg:grid-cols-4 gap-4 mb-16">
+                            <Card className="border-2 border-brand-700">
+                                <div className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                    <p className="text-sm/6 font-medium">
+                                        Total Events
+                                    </p>
+                                    <BarChart className="size-4 text-muted-foreground"/>
+                                </div>
 
+                                <div>
+                                    <p className="text-2xl font-bold">
+                                        {data?.eventsCount || 0}
+                                    </p>
+                                    <p className="text-xs/5 text-muted-foreground">
+                                        Events{" "}
+                                        {activeTab === "today" 
+                                        ? "today" 
+                                        : activeTab === "week"
+                                        ? "this week"
+                                        : "this month"}
+                                    </p>
+                                </div>
+                            </Card>
+
+                            <NumericFieldSumCards/>
+                        </div>
+                    </TabsContent>
+            </Tabs>
         </div>
     )
 }
